0ec0ad417697b4d8e859a0299d9884921b766a5d
LeshchyshynB <bogdanleshchyshyn99@gmail.com>
2024-02-24 10:32:46 +0200

    init venv and add requirements

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ pygame.init()
 
 prev_time = time.time()
 
-# while True:
-# 	dt = time.time() - prev_time
-# 	prev_time = time.time()
+while True:
+	dt = time.time() - prev_time
+	prev_time = time.time()
 
+	
